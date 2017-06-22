e4634fe248974b7c90f8b8c51ff3dd6736e415ae
kvsemenoff <kvsemenoff@mail.ru>
2017-06-22 13:44:05 +0400

    f

--- includes/header.php
+++ includes/header.php
@@ -1,14 +1,12 @@
-
 	<link rel="shortcut icon" href="favicon.png" />
-
+	
+	<!-- styles start -->
 	<link rel="stylesheet" href="libs/bootstrap/bootstrap-grid-3.3.1.min.css" />
 	<link rel="stylesheet" href="libs/fancybox/jquery.fancybox.css" />
 	<link rel="stylesheet" href="libs/owl.carousel/assets/owl.carousel.css" />	
-
 	<link rel="stylesheet" href="css/fonts.css">
 	<link rel="stylesheet" href="css/style.css">
 	<link rel="stylesheet" href="css/popup.css">
-
 	<link rel="stylesheet" href="css/style-db.css">
 	<link rel="stylesheet" href="css/style-dc.css">
 	<link rel="stylesheet" href="css/style-dd.css">
@@ -18,5 +16,37 @@
 	<link rel="stylesheet" href="css/style-dh.css">
 	<link rel="stylesheet" href="css/style-di.css">
 	<link rel="stylesheet" href="css/style-dj.css">
+	<link rel="stylesheet" href="block/a/style.css">
+	<link rel="stylesheet" href="block/brearcrumbs/style.css">
+	<link rel="stylesheet" href="block/burg-title/style.css">
+	<link rel="stylesheet" href="block/button/style.css">
+	<link rel="stylesheet" href="block/button-question/style.css">
+	<link rel="stylesheet" href="block/catalog-list/style.css">
+	<link rel="stylesheet" href="block/inputsubmit/style.css">
+	<link rel="stylesheet" href="block/inputtext/style.css">
+	<link rel="stylesheet" href="block/item-row/style.css">
+	<link rel="stylesheet" href="block/search-box/style.css">
+	<link rel="stylesheet" href="block/select/style.css">
+	<link rel="stylesheet" href="block/table/style.css">
+	<link rel="stylesheet" href="block/tech-doc/style.css">
+	<link rel="stylesheet" href="block/textarea/style.css">
+	<link rel="stylesheet" href="block/watch-more/style.css">
+	<!-- styles end -->
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
 	
 

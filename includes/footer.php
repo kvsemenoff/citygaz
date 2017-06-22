2ab377e9bdfff277e00b918b7388b58d080b0006
boka <web_masters_07@mail.ru>
2017-06-22 14:57:58 +0300

    footer-bottom

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,47 @@
-
+<div class="footer-bottom">
+	<div class="container">
+		
+		<div class="col-md-3 col-sm-6 col-xs-12">
+			<div class="box">
+				<ul>
+					<li><a href="#">О комании</a></li>
+					<li><a href="#">Монтаж</a></li>
+					<li><a href="#">Ремонт и обслуживание</a></li>
+					<li><a href="#">Как заказать</a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="col-md-3 col-sm-6 col-xs-12">
+			<div class="box">
+				<ul>
+					<li><a href="#">Доставка и оплата</a></li>
+					<li><a href="#">Документы</a></li>
+					<li><a href="#">Контакты</a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="clear"></div>
+		<div class="col-md-3 col-sm-6 col-xs-12">
+			<div class="pd-t box">
+				<a href="#" class="citchen">Запчасти<br>к плитам</a><br>
+				<a href="#" class="cotl">Запчасти<br>к котлам</a>
+			</div>
+		</div>
+		<div class="col-md-3 col-sm-6 col-xs-12">
+			<div class="pd-t box">
+				<a href="#" class="electro">Запчасти<br>к электрооборудованию</a><br>
+				<a href="#" class="gas">Запчасти<br>к водонагревателям</a>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="footer-bottom2">
+	<div class="container">
+		<div class="col-md-12">
+			<p>Разработано в <a href="#">Insite</a></p>
+		</div>
+	</div>
+</div>
 
 <?php require_once('popup.php'); ?>
 

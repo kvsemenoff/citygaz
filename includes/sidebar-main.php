476532fc63d0077a9f64e8f7570437be1218ae4a
engine77 <zaur.1kager@gmail.com>
2017-06-23 11:57:32 +0400

    sidebar

--- includes/sidebar-main.php
+++ includes/sidebar-main.php
@@ -93,7 +93,83 @@
 
 	<div class="burg-title">Бренды</div>	
 	<div class="clearfix"></div>
-
 	
+	<div class="brand">
+		<div class="brand__box">
+			<div class="brand__img">
+				<img src="img/brend1.png" alt="">
+			</div>
+			<a href="#">Ariston</a>
+		</div>
+		<div class="brand__box">
+			<div class="brand__img">
+				<img src="img/brend2.png" alt="">
+			</div>
+			<a href="#">Baxi</a>
+		</div>
+		<div class="brand__box">
+			<div class="brand__img">
+				<img src="img/brend3.png" alt="">
+			</div>
+			<a href="#">Bosch-Junkers</a>
+		</div>
+		<div class="brand__box">
+			<div class="brand__img">
+				<img src="img/brend4.png" alt="">
+			</div>
+			<a href="#">Buderus</a>
+		</div>
+		<div class="brand__box">
+			<div class="brand__img">
+				<img src="img/brend5.png" alt="">
+			</div>
+			<a href="#">Attack</a>
+		</div>
+		<div class="brand__box">
+			<div class="brand__img">
+				<img src="img/brend6.png" alt="">
+			</div>
+			<a href="#">Electrolux</a>
+		</div>
+		<div class="brand__box">
+			<div class="brand__img">
+				<img src="img/brend7.png" alt="">
+			</div>
+			<a href="#">Elsotherm</a>
+		</div>
+		<div class="brand__box">
+			<div class="brand__img">
+				<img src="img/brend8.png" alt="">
+			</div>
+			<a href="#">Ferroli</a>
+		</div>
+		<div class="brand__box">
+			<div class="brand__img">
+				<img src="img/brend9.png" alt="">
+			</div>
+			<a href="#">Fondital</a>
+		</div>
+		<div class="brand__box">
+			<div class="brand__img">
+				<img src="img/brend10.png" alt="">
+			</div>
+			<a href="#">Gazlux</a>
+		</div>
+		<div class="brand__box">
+			<div class="brand__img">
+				<img src="img/brend11.png" alt="">
+			</div>
+			<a href="#">Immergas</a>
+		</div>
+		<div class="brand__allbrends">			
+			<a href="#">Все бренды</a>
+		</div>
+	</div>
+	
+	<div class="sidebar-buttons">
+		<a href="#">Продажа газового оборудования </a>
+		<a href="#">Жалобы и предложения клиентов</a>
+	</div>
+
 </div>
 

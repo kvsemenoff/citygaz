487118395e15ea82560025eeb8ce94d0077a7775
boka <web_masters_07@mail.ru>
2017-06-23 11:40:03 +0300

    footer

--- includes/footer.php
+++ includes/footer.php
@@ -3,33 +3,33 @@
 		<div class="col-md-3 col-sm-6 col-xs-12">
 			<div class="box">
 				<ul>
-					<li><a href="#">О комании</a></li>
-					<li><a href="#">Монтаж</a></li>
-					<li><a href="#">Ремонт и обслуживание</a></li>
-					<li><a href="#">Как заказать</a></li>
+					<li><a href="#" class="a">О комании</a></li>
+					<li><a href="#" class="a">Монтаж</a></li>
+					<li><a href="#" class="a">Ремонт и обслуживание</a></li>
+					<li><a href="#" class="a">Как заказать</a></li>
 				</ul>
 			</div>
 		</div>
 		<div class="col-md-3 col-sm-6 col-xs-12">
 			<div class="box">
 				<ul>
-					<li><a href="#">Доставка и оплата</a></li>
-					<li><a href="#">Документы</a></li>
-					<li><a href="#">Контакты</a></li>
+					<li><a href="#" class="a">Доставка и оплата</a></li>
+					<li><a href="#" class="a">Документы</a></li>
+					<li><a href="#" class="a">Контакты</a></li>
 				</ul>
 			</div>
 		</div>
 		<div class="clear"></div>
 		<div class="col-md-3 col-sm-6 col-xs-12">
 			<div class="pd-t box">
-				<a href="#" class="citchen">Запчасти<br>к плитам</a><br>
-				<a href="#" class="cotl">Запчасти<br>к котлам</a>
+				<a href="#" class="citchen a">Запчасти<br>к плитам</a><br>
+				<a href="#" class="cotl a">Запчасти<br>к котлам</a>
 			</div>
 		</div>
 		<div class="col-md-3 col-sm-6 col-xs-12">
 			<div class="pd-t box">
-				<a href="#" class="electro">Запчасти<br>к электрооборудованию</a><br>
-				<a href="#" class="gas">Запчасти<br>к водонагревателям</a>
+				<a href="#" class="electro a">Запчасти<br>к электрооборудованию</a><br>
+				<a href="#" class="gas a">Запчасти<br>к водонагревателям</a>
 			</div>
 		</div>
 	</div>

a63461f59531be0f99d71627553116a8aa9eeeea
engine77 <zaur.1kager@gmail.com>
2017-06-23 13:08:27 +0400

    sidebar

---
+++ catalog.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Главная</title>
+	<?php require_once('includes/header.php'); ?>
+	<div class="wrapper">
+		<div class="content">
+			<?php require_once('includes/header_top.php'); ?>
+			<div class="container">
+				<?php require_once('includes/sidebar-catalog.php'); ?>
+			</div>
+		</div>
+		<div class="footer">
+			<?php require_once('includes/footer_top.php'); ?>
+			<?php require_once('includes/footer.php'); ?>
+		</div>
+	</div>
+</body>
+</html>
+

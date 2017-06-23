d23f4c1569c5be8b504ce246b32e00c9dd0bf67d
boka <web_masters_07@mail.ru>
2017-06-23 12:08:33 +0300

    footer

--- includes/header_top.php
+++ includes/header_top.php
@@ -1,24 +1,48 @@
 <div class="header-top">
 	<div class="container">
-		<div class="burger">
-			<a href="#"><span>Меню</span></a>
+		<div class="header_box">
+			<div class="header-top_inner">
+				<div class="one">
+					<img src="../img/header.png" alt="img">
+					<p>городская <br> газовая служба</p>
+					<div class="clearfix"></div>
+				</div>
+				<div class="two">
+					<p>
+						<span>г. Нижний Новгород,</span><br>
+						ул. Коммунистическая д. 77
+					</p>
+				</div>
+				<div class="three">
+					<p>
+						диспетчер: <span class="first">(831)</span>  <span class="second">4 - 139 - 169</span><br>
+						тел/факс <span class="first">(831)</span>  <span class="second">202 - 90 - 33</span>
+					</p>
+				</div>
+				<div class="four">
+					<a href="#" class="button-question">Задать вопрос</a>
+				</div>	
+			</div>
+			<div class="burger">
+				<a href="#"><span>Меню</span></a>
+			</div>
+			<ul class="main-menu">
+				<li><a href="#">О КОМПАНИИ</a></li>
+				<li><a href="#">МОНТАЖ</a></li>
+				<li><a href="#">РЕМОНТ И ОБСЛУЖИВАНИЕ</a></li>
+				<li><a href="#">КАК ЗАКАЗАТЬ</a></li>
+				<li class="active">
+					<a href="#" class="active-bg">ДОСТАВКА И ОПЛАТА</a>
+					<ul class="main-manu2">
+						<li><a href="#">Доставка курьером</a></li>
+						<li><a href="#">Доставка почтой</a></li>
+						<li><a href="#">Самовывоз</a></li>
+					</ul>
+				</li>
+				<li><a href="#">ДОКУМЕНТЫ</a></li>
+				<li><a href="#">КОНТАКТЫ</a></li>
+			</ul>
 		</div>
-		<ul class="main-menu">
-			<li><a href="#">О КОМПАНИИ</a></li>
-			<li><a href="#">МОНТАЖ</a></li>
-			<li><a href="#">РЕМОНТ И ОБСЛУЖИВАНИЕ</a></li>
-			<li><a href="#">КАК ЗАКАЗАТЬ</a></li>
-			<li class="active">
-				<a href="#" class="active-bg">ДОСТАВКА И ОПЛАТА</a>
-				<ul class="main-manu2">
-					<li><a href="#">Доставка курьером</a></li>
-					<li><a href="#">Доставка почтой</a></li>
-					<li><a href="#">Самовывоз</a></li>
-				</ul>
-			</li>
-			<li><a href="#">ДОКУМЕНТЫ</a></li>
-			<li><a href="#">КОНТАКТЫ</a></li>
-		</ul>
 	</div>
 </div>
 

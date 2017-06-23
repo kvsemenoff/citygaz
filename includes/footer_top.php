ecf6e47cd6b3351d1072fb07bbcfba3731b247bc
kvsemenoff <kvsemenoff@mail.ru>
2017-06-23 17:02:15 +0400

    d

--- includes/footer_top.php
+++ includes/footer_top.php
@@ -1,43 +1,37 @@
-<div class="footer-top">
+<div class="info-row info-row_bottom">
 	<div class="container">
-		<div class="col-md-3">
-			<div class="one">
-				<img src="../img/header.png" alt="img">
-				<p>городская <br> газовая служба</p>
-				<div class="clearfix"></div>
+		<div class="row">
+			<div class="col-md-4 col-sm-12">
+				<a href="/">
+					<div class="logo-box">
+						<img src="img/header.png" alt="CityGaz">	
+						<div class="slogan">городская <br> газовая служба</div>
+					</div>
+				</a>
 			</div>
-		</div>	
-		<div class="col-md-3">
-			<div class="two">
-				<p>
-					<span>г. Нижний Новгород,</span><br>
-					ул. Коммунистическая д. 77
-				</p>
+			<div class="col-md-5 col-sm-12">
+				<div class="contact-box">
+					<div class="contact-address">
+						<strong>г. Нижний Новгород,</strong> <br>
+						
+						ул. Коммунистическая д. 77
+					</div>
+					<div class="contact-phones">
+						<span>диспетчер:</span> <span>(831) <strong>4 - 139 - 169</strong></span><br>
+						<span>тел/факс</span> &nbsp; &nbsp;  <span>(831) <strong>202 - 90 - 33</strong></span>
+					</div>
+				</div>
 			</div>
-		</div>
-		<div class="col-md-3">
-			<div class="three">
-				<p>
-					диспетчер: <span class="first">(831)</span>  <span class="second">4 - 139 - 169</span><br>
-					тел/факс <span class="first">(831)</span>  <span class="second">202 - 90 - 33</span>
-				</p>
+			<div class="col-md-3 col-sm-12">
+				<div class="right-box">
+					<a href="#" class="button-question">Задать вопрос</a>
+				</div>
 			</div>
-		</div>
-		<div class="col-md-3">
-			<div class="four">
-				<a href="#" class="button-question">Задать вопрос</a>
-			</div>
+			<div class="clearfix"></div>
 		</div>
 	</div>
 </div>
-	
-	
-	
-	
-	
-	
-	
-	
+
 	
 	
 	
--- includes/header_top.php
+++ includes/header_top.php
@@ -1,65 +1,65 @@
-<div class="header-top">
+<div class="info-row info-row_top">
 	<div class="container">
-		<div class="header_box">
-			<div class="header-top_inner">
-				<div class="col-md-4">
-					<div class="one">
-						<img src="../img/header.png" alt="img">
-						<p>городская <br> газовая служба</p>
-						<!-- <div class="clearfix"></div> -->
+		<div class="row">
+			<div class="col-md-4 col-sm-12">
+				<div class="logo-box">
+					<a href="/">
+						<img src="img/header.png" alt="CityGaz">	
+						
+					</a>
+					<div class="slogan">городская <br> газовая служба</div>
+				</div>
+			</div>
+			<div class="col-md-4 col-sm-12">
+				<div class="contact-box hidden-md">
+					<div class="contact-address">
+						<strong>г. Нижний Новгород,</strong> <br>
+						
+						ул. Коммунистическая д. 77
+					</div>
+					<div class="contact-phones">
+						диспетчер: <span>(831) <strong>4 - 139 - 169</strong></span><br>
+						тел/факс <span>(831) <strong>202 - 90 - 33</strong></span>
 					</div>
 				</div>
-				<div class="col-md-4">
-					<div class="two">
-						<p>
-							<span>г. Нижний Новгород,</span><br>
-							ул. Коммунистическая д. 77
-						</p>
-					</div>
-					<div class="three">
-						<p>
-							диспетчер: <span class="first">(831)</span>  <span class="second">4 - 139 - 169</span><br>
-							тел/факс <span class="first">(831)</span>  <span class="second">202 - 90 - 33</span>
-						</p>
-					</div>
-				</div>
-				<div class="col-md-4">
-					<div class="four">
-						<a href="#" class="button-question">Задать вопрос</a>
-					</div>	
-					<div class="five">
-						<div class="cart">
-							<div class="cart-count-box">
-								<span class="count">0</span>
-							</div>
-							<div class="cart-text">
-								Нет <br>товаров
-							</div>
-							<div class="clearfix"></div>
+			</div>
+			<div class="col-md-4 col-sm-12">
+				<div class="right-box">
+					<a href="#" class="button-question">Задать вопрос</a>
+					<div class="cart">
+						<div class="cart-count-box">
+							<span class="count">0</span>
 						</div>
+						<div class="cart-text">
+							Нет <br>товаров
+						</div>
+						<div class="clearfix"></div>
 					</div>
 				</div>
 			</div>
-			<div class="burger">
-				<a href="#"><span>Меню</span></a>
-			</div>
-			<ul class="main-menu">
-				<li><a href="#">О КОМПАНИИ</a></li>
-				<li><a href="#">МОНТАЖ</a></li>
-				<li><a href="#">РЕМОНТ И ОБСЛУЖИВАНИЕ</a></li>
-				<li><a href="#">КАК ЗАКАЗАТЬ</a></li>
-				<li class="active">
-					<a href="#" class="active-bg">ДОСТАВКА И ОПЛАТА</a>
-					<ul class="main-manu2">
-						<li><a href="#">Доставка курьером</a></li>
-						<li><a href="#">Доставка почтой</a></li>
-						<li><a href="#">Самовывоз</a></li>
-					</ul>
-				</li>
-				<li><a href="#">ДОКУМЕНТЫ</a></li>
-				<li><a href="#">КОНТАКТЫ</a></li>
-			</ul>
+			<div class="clearfix"></div>
 		</div>
+	</div>
+	<a href="#mobile" data-modal="mobile" class="burger" ><i class="fa fa-bars" aria-hidden="true"></i></a>
+</div>
+<div class="menu-box">
+	<div class="container">
+		<ul class="main-menu">
+			<li><a href="#">О КОМПАНИИ</a></li>
+			<li><a href="#">МОНТАЖ</a></li>
+			<li><a href="#">РЕМОНТ И ОБСЛУЖИВАНИЕ</a></li>
+			<li><a href="#">КАК ЗАКАЗАТЬ</a></li>
+			<li class="active">
+				<a href="#" class="active-bg">ДОСТАВКА И ОПЛАТА</a>
+				<ul class="main-manu2">
+					<li><a href="#">Доставка курьером</a></li>
+					<li><a href="#">Доставка почтой</a></li>
+					<li><a href="#">Самовывоз</a></li>
+				</ul>
+			</li>
+			<li><a href="#">ДОКУМЕНТЫ</a></li>
+			<li><a href="#">КОНТАКТЫ</a></li>
+		</ul>
 	</div>
 </div>
 
@@ -69,11 +69,36 @@
 
 
 
+<!-- 
 
 
+<div class="header_box">
+			<div class="header-top_inner">
+					
+				<div class="burger">
+					<a href="#"><span>Меню</span></a>
+				</div>
+				<ul class="main-menu">
+					<li><a href="#">О КОМПАНИИ</a></li>
+					<li><a href="#">МОНТАЖ</a></li>
+					<li><a href="#">РЕМОНТ И ОБСЛУЖИВАНИЕ</a></li>
+					<li><a href="#">КАК ЗАКАЗАТЬ</a></li>
+					<li class="active">
+						<a href="#" class="active-bg">ДОСТАВКА И ОПЛАТА</a>
+						<ul class="main-manu2">
+							<li><a href="#">Доставка курьером</a></li>
+							<li><a href="#">Доставка почтой</a></li>
+							<li><a href="#">Самовывоз</a></li>
+						</ul>
+					</li>
+					<li><a href="#">ДОКУМЕНТЫ</a></li>
+					<li><a href="#">КОНТАКТЫ</a></li>
+				</ul>
+			</div>
+		</div>
 
 
-
+ -->
 
 
 

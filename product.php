5571614820a57ad56008a216d8b23a7f1d8107a1
engine77 <zaur.1kager@gmail.com>
2017-06-24 14:20:51 +0400

    bread

---
+++ product.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Главная</title>
+	<?php require_once('includes/header.php'); ?>
+	<div class="wrapper">
+		<div class="content">
+			<?php require_once('includes/header_top.php'); ?>
+			
+			<div class="container">
+				<div class="product">
+					<div class="product__topbox">
+						<a href="#" class="product__catalog">Каталог товаров</a>
+						<ul class="brearcrumbs">
+							<li><a href="#">Главная</a></li>
+							<li><span>Запчасти к водонагревателям</span></li>
+						</ul>
+						<div class="clear"></div>
+					</div>
+
+
+					<div class="product__block">
+						<div class="product__leftbox">
+							<span>Акция</span>
+							<div class="product__imgbox">
+								<img src="img/product1.jpg" alt="">
+							</div>
+							<div class="product__galerybox">
+								<a href="img/product1.jpg">
+									<img src="img/productgalery1.jpg" alt="">
+								</a>
+								<a href="img/product1.jpg">
+									<img src="img/productgalery2.jpg" alt="">
+								</a>
+								<a href="img/product1.jpg">
+									<img src="img/productgalery3.jpg" alt="">
+								</a>
+							</div>
+						</div>
+						
+						<div class="product__rightbox">
+							<h2>Водогазовый блок Mertic для колонки Mora</h2>
+							<div class="product__articul">
+								<span>В наличии</span>
+								<span>Артикул: <b>24687965</b></span>
+							</div>
+							<span class="product__brend">Производитель: <b>Ariston</b></span>
+							<span class="product__characteristic">Описание:</span>
+							<ul class="product__charbox">
+								<li>Общая длина - 390мм.</li>
+								<li>Длинна рабочей части - 270мм.</li>
+							</ul>
+							<p class="product__txtbox">Теплообменник битермический Nova Florida Vela 
+								Compact / Fondital Panarea Compact Art. 6SCAMBIM04
+								Теплообменник битермический Nova Florida Vela 
+								Compact / Fondital Panarea Compact Art. 6SCAMBIM04
+							</p>
+
+							<div class="product__pricebox">
+								<span class="product__oldprice">8 600 .-</span>
+								<span class="product__newprice">5 200 .-</span>
+								<a href="#">Купить</a>
+							</div>
+						</div>
+
+					</div>
+
+
+
+
+
+
+
+				</div>
+			</div>
+		</div>
+		<div class="footer">
+			<?php require_once('includes/footer_top.php'); ?>
+			<?php require_once('includes/footer.php'); ?>
+		</div>
+	</div>
+</body>
+</html>
+

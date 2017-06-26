5efa37d781bb4b681a62b2bed641c88ffa6013d6
Kirill Solovyev <Kirill.Solovyev.98@gmail.com>
2017-06-26 12:46:12 +0600

    d

--- includes/popup.php
+++ includes/popup.php
@@ -1,103 +1,352 @@
-<div class="modal-window" id="order">
-	<span class="modal-close"></span>
-	<h3>Заказать ремонт</h3>
-	<form name='popup_form'>
-	<input type="text" placeholder="Ваше имя *" name="uname" required>
-	<input type="text" placeholder="Телефон *" name="phone" required>
-	<input type="text" placeholder="Адрес" name='address'>
-	<textarea placeholder="Сообщение" name='text'></textarea>
-	<input type="submit" value='Заказать ремонт'></input>
-	</form>
+<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Главная</title>
+	<?php require_once('includes/header.php'); ?>
+	<div class="wrapper">
+		<div class="content">
+			<?php require_once('includes/header_top.php'); ?>
+			<div class="clearfix"></div>
+			<div class="container-main">
+				<div class="left-side">
+					<?php require_once('includes/sidebar-main.php'); ?>
+				</div>
+				<div class="right-side">
+					<?php require_once('includes/slider-main.php'); ?>
+					<div class="clearfix"></div>
+					<div class='catalog'>
+						<div class="cat-col">
 
-</div>
+							<div class="cat-item">
+								<div class="cat-item_one catid">
+									<p class='catalog_title catalog_title_cooker'>Запчасти <br> к плитам</p>
+								
+									<ul class="catalog-list">
+										<li><a href="#">Программаторы</a></li>
+										<li><a href="#">Расширительные баки</a></li>
+										<li><a href="#">Реле протока, группы ГВС</a></li>
+										<li><a href="#">Теплообменники</a></li>
+										<li><a href="#">Термостаты безопасности</a></li>
+										<li><a href="#">Программаторы</a></li>
+										<li><a href="#">Расширительные баки</a></li>
+										<li><a href="#">Реле протока, группы ГВС</a></li>
+										<li><a href="#">Теплообменники</a></li>
+										<li><a href="#">Термостаты безопасности</a></li>
+										<li><a href="#">Программаторы</a></li>
+										<li><a href="#">Расширительные баки</a></li>
+										<li><a href="#">Реле протока, группы ГВС</a></li>
+										<li><a href="#">Теплообменники</a></li>
+										<li><a href="#">Термостаты безопасности</a></li>
+										<li><a href="#">Термостаты безопасности</a></li>
+										<li><a href="#">Теплообменники</a></li>
+										<li><a href="#">Термостаты безопасности</a></li>
+										<li><a href="#">Расширительные баки</a></li>
+										<li><a href="#">Реле протока, группы ГВС</a></li>
+										<li><a href="#">Теплообменники</a></li>
+										<li><a href="#">Термостаты безопасности</a></li>
+										<li><a href="#">Расширительные баки</a></li>
+										<li><a href="#">Реле протока, группы ГВС</a></li>
+										<li><a href="#">Теплообменники</a></li>
+										<li><a href="#">Термостаты безопасности</a></li>
+										<li><a href="#">Теплообменники</a></li>
+										<li><a href="#">Термостаты безопасности</a></li>
+										<li><a href="#">Реле протока, группы ГВС</a></li>
+										<li><a href="#">Теплообменники</a></li>
+										<li><a href="#">Термостаты безопасности</a></li>
+										<li><a href="#">Программаторы</a></li>
+										<li><a href="#">Расширительные баки</a></li>
+										<li><a href="#">Реле протока, группы ГВС</a></li>
+										<li><a href="#">Теплообменники</a></li>
+										<li><a href="#">Термостаты безопасности</a></li>
+										<li><a href="#">Термостаты безопасности</a></li>
+									</ul> 
+									<a href='#' class='watch-more watch-more_small'>Смотреть ещё</a>
+								</div>
+								<div class="cat-item-inner">
+									<p class='catalog_title catalog_title_cooker'>Запчасти <br> к плитам</p>
+									<ul class="catalog-list"></ul> 
+								</div>
+							</div>
+						</div>
+						<div class="cat-col">
+							<div class="cat-item">
+								<div class="cat-item_one catid">
+									<p class='catalog_title catalog_title_htautomation'>Запчасти <br> к котлам</p>
+									<ul class="catalog-list">
+										<li><a href="#">Программаторы</a></li>
+										<li><a href="#">Расширительные баки</a></li>
+										<li><a href="#">Реле протока, группы ГВС</a></li>
+										<li><a href="#">Теплообменники</a></li>
+										<li><a href="#">Термостаты безопасности</a></li>
+										<li><a href="#">Расширительные баки</a></li>
+										<li><a href="#">Реле протока, группы ГВС</a></li>
+										<li><a href="#">Теплообменники</a></li>
+										<li><a href="#">Термостаты безопасности</a></li>
+										<li><a href="#">Расширительные баки</a></li>
+										<li><a href="#">Реле протока, группы ГВС</a></li>
+										<li><a href="#">Теплообменники</a></li>
+										<li><a href="#">Термостаты безопасности</a></li>
+										<li><a href="#">Расширительные баки</a></li>
+										<li><a href="#">Реле протока, группы ГВС</a></li>
+										<li><a href="#">Теплообменники</a></li>
+										<li><a href="#">Термостаты безопасности</a></li>
+										<li><a href="#">Расширительные баки</a></li>
+										<li><a href="#">Реле протока, группы ГВС</a></li>
+										<li><a href="#">Теплообменники</a></li>
+										<li><a href="#">Термостаты безопасности</a></li>
+										<li><a href="#">Теплообменники</a></li>
+										<li><a href="#">Термостаты безопасности</a></li>
+									</ul> 
+									<a href='#' class='watch-more watch-more_small'>Смотреть ещё</a>
+								</div>
+								<div class="cat-item-inner">
+									<p class='catalog_title catalog_title_htautomation'>Запчасти <br> к котлам</p>
+									<ul class="catalog-list"></ul> 
+								</div>
+							</div>
+						</div>
+						<div class="cat-col">
+							<div class="cat-item">
+								<div class="cat-item_two catid">
+									<p class='catalog_title catalog_title_pulse'>Запчасти к <br>электрооборудованию</p>
+									<ul class="catalog-list">
+										<li><a href="#">Программаторы</a></li>
+										<li><a href="#">Расширительные баки</a></li>
+										<li><a href="#">Реле протока, группы ГВС</a></li>
+										<li><a href="#">Расширительные баки</a></li>
+										<li><a href="#">Реле протока, группы ГВС</a></li>
+									</ul> 
+								</div>
+								<div class="cat-item-inner">
+									<p class='catalog_title catalog_title_pulse'>Запчасти к <br>электрооборудованию</p>
+									<ul class="catalog-list"></ul> 
+								</div>
+							</div>
+							<div class="cat-item">
+								<div class="cat-item_three catid">
+									<p class='catalog_title catalog_title_gas-blue'>Запчасти к <br> водонагревателям</p>
+									<ul class="catalog-list">
+										<li><a href="#">Программаторы</a></li>
+										<li><a href="#">Расширительные баки</a></li>
+										<li><a href="#">Реле протока, группы ГВС</a></li>
+										<li><a href="#">Теплообменники</a></li>
+										<li><a href="#">Термостаты безопасности</a></li>
+										<li><a href="#">Теплообменники</a></li>
+										<li><a href="#">Термостаты безопасности</a></li>
+										<li><a href="#">Теплообменники</a></li>
+										<li><a href="#">Термостаты безопасности</a></li>
+									</ul> 
+									<a href='#' class='watch-more watch-more_small'>Смотреть ещё</a>
+								</div>
+								<div class="cat-item-inner">
+									<p class='catalog_title catalog_title_pulse'>Запчасти к <br>электрооборудованию</p>
+									<ul class="catalog-list"></ul> 
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="clear"></div>
+					<!-- Popups  -->
 
-<div class="modal-window" id="compl">
-	<span class="modal-close"></span>
-	<h3>Жалобы клиентов</h3>
-	<form name='popup_form'>
-	<input type="text" placeholder="Ваше имя *" name="uname" required>
-	<input type="text" placeholder="Телефон *" name="phone" required>
-	<input type="text" placeholder="Ваш Email" name='email'>
-	<textarea placeholder="Текст жалобы" name='text'></textarea>
-	<input type="submit" value='Отправить жалобу'></input>
-	</form>
+					<a href="#order" data-modal="modal" >Заказать ремонт   .</a>
+					<a href="#compl" data-modal="modal" >Пожаловаться   .</a>
+					<a href="#ask-question" data-modal="modal" >Задать вопрос   .</a>
+					<a href="#description" data-modal="modal" >Неточность описания  .</a>
+					<a href='#bucket_popup' data-modal="modal">Корзина   .</a>
+					<a href='#thanks' data-modal="modal">Спасибо</a>
 
-</div>
+					<div class="modal-window" id="order">
+						<span class="modal-close"></span>
+						<h3>Заказать ремонт</h3>
+						<form name='popup_form'>
+						<input type="text" placeholder="Ваше имя *" name="uname" required>
+						<input type="text" placeholder="Телефон *" name="phone" required>
+						<input type="text" placeholder="Адрес" name='address'>
+						<textarea placeholder="Сообщение" name='text'></textarea>
+						<input type="submit" value='Заказать ремонт'></input>
+						</form>
 
-<div class="modal-window" id="ask-question">
-	<span class="modal-close"></span>
-	<h3>Задать вопрос</h3>
-	<form name='popup_form'>
-	<input type="text" placeholder="Ваше имя *" name="uname" required>
-	<input type="text" placeholder="Телефон " name="phone">
-	<input type="text" placeholder="Ваш Email *" name='email' required>
-	<textarea placeholder="Вопрос" name='text'></textarea>
-	<input type="submit" value='Задать вопрос'></input>
-	</form>
+					</div>
 
-</div>
+					<div class="modal-window" id="compl">
+						<span class="modal-close"></span>
+						<h3>Жалобы клиентов</h3>
+						<form name='popup_form'>
+						<input type="text" placeholder="Ваше имя *" name="uname" required>
+						<input type="text" placeholder="Телефон *" name="phone" required>
+						<input type="text" placeholder="Ваш Email" name='email'>
+						<textarea placeholder="Текст жалобы" name='text'></textarea>
+						<input type="submit" value='Отправить жалобу'></input>
+						</form>
 
-<div class="modal-window" id="description">
-	<span class="modal-close"></span>
-	<h3>Неточность описания</h3>
-	<form name='popup_form'>
-	<input type="text" placeholder="Ваше имя" name="uname">
-	<input type="text" placeholder="Ваш Email" name='email'>
-	<textarea placeholder="Сообщение *" name='text' required></textarea>
-	<input type="submit" value='Отправит на проверку' ></input>
-	</form>
+					</div>
 
-</div>
+					<div class="modal-window" id="ask-question">
+						<span class="modal-close"></span>
+						<h3>Задать вопрос</h3>
+						<form name='popup_form'>
+						<input type="text" placeholder="Ваше имя *" name="uname" required>
+						<input type="text" placeholder="Телефон " name="phone">
+						<input type="text" placeholder="Ваш Email *" name='email' required>
+						<textarea placeholder="Вопрос" name='text'></textarea>
+						<input type="submit" value='Задать вопрос'></input>
+						</form>
 
-<div class="modal-window" id="bucket_popup">
-	<h3>Вы добавили в корзину</h3>
-	<div>
-		<div>
-		<p class='article'>Артикул 1234567</p>
-		<a href='#'>Запчасти для газовых колонок:комплектующие</a>
-	</div>
-		<img src='../img/5665220.png' />
-	</div>
-	<input type="submit" value='Оформить заказ' ></input>
-	<a class="modal-close">Продолжить покупки</a>
+					</div>
 
-</div>
+					<div class="modal-window" id="description">
+						<span class="modal-close"></span>
+						<h3>Неточность описания</h3>
+						<form name='popup_form'>
+						<input type="text" placeholder="Ваше имя" name="uname">
+						<input type="text" placeholder="Ваш Email" name='email'>
+						<textarea placeholder="Сообщение *" name='text' required></textarea>
+						<input type="submit" value='Отправит на проверку' ></input>
+						</form>
 
-<div class="modal-window" id="thanks">
-	<span class="modal-close"></span>
-	<br>
-	<span class="popup-title">Спасибо за вашу заявку!</span>
-	<p>
-		В ближайшее время наш менеджер <br> свяжется с Вами.
-	</p>	
-</div>
+					</div>
 
-<div class="mobile-window" id="mobile">
-	<span class="modal-close"><i class="fa fa-times" aria-hidden="true"></i></span>
-	<div class="mobile-line"></div>
-		<div class="mobile-logo">
-			<a href="/">
-				<img src="../img/header.png" alt="CityGaz">	
-			</a>		
+					<div class="modal-window" id="bucket_popup">
+						<h3>Вы добавили в корзину</h3>
+						<div>
+							<div>
+							<p class='article'>Артикул 1234567</p>
+							<a href='#'>Запчасти для газовых колонок:комплектующие</a>
+						</div>
+							<img src='../img/5665220.png' />
+						</div>
+						<input type="submit" value='Оформить заказ' ></input>
+						<a class="modal-close">Продолжить покупки</a>
+
+					</div>
+
+					<div class="modal-window" id="thanks">
+						<span class="modal-close"></span>
+						<br>
+						<span class="popup-title">Спасибо за вашу заявку!</span>
+						<p>
+							В ближайшее время наш менеджер <br> свяжется с Вами.
+						</p>	
+					</div>
+
+					<div class="mobile-window" id="mobile">
+						<span class="modal-close"><i class="fa fa-times" aria-hidden="true"></i></span>
+						<div class="mobile-line"></div>
+							<div class="mobile-logo">
+								<a href="/">
+									<img src="../img/header.png" alt="CityGaz">	
+								</a>		
+							</div>
+						<ul class="mobile-main-menu">
+							<li><a href="#">О компании</a></li>
+							<li><a href="#">Монтаж</a></li>
+							<li><a href="#">Ремонт и обслуживаение</a></li>
+							<li><a href="#">Как заказать</a></li>
+							<li class="active">
+								<a href="#" class="active-bg">Доставка и оплата</a>
+								<ul class="main-manu2">
+									<li><a href="#">Доставка курьером</a></li>
+									<li><a href="#">Доставка почтой</a></li>
+									<li><a href="#">Самовывоз</a></li>
+								</ul>
+							</li>
+							<li><a href="#">Документы</a></li>
+							<li><a href="#">Контакты</a></li>
+						</ul>
+
+					</div>
+
+					<!-- Popups -->
+
+					<!-- Хиты продаж. Начало -->
+
+					<div class="item-row">
+						<div class="burg-title">Хит продаж</div>
+						<a href="#" class="watch-more">Смотреть ещё</a>
+					</div>
+					<div class="tovar-row">
+						<div class="col">
+							<div class="tovar-item">
+								<a href='#'>
+									<div>
+										<img src='img/engine.png'/>
+									</div>
+								</a>
+								<a href='#' class="item-title">Газовый клапан 820 NOVA mv. 0.820.303 цена, купить Днепр</a>
+								<p class='old-price'><span>8 600</span></p>
+								<p class='price orange'><span>5 200</span></p>
+							</div>
+						</div>
+						<div class="col">
+							<div class='tovar-item'>
+								<a href='#'>
+									<div>
+										<img src='img/cooler.png'/>
+									</div>
+								</a>
+								<a href='#' class="item-title">Газовый клапан 820 NOVA mv. 0.820.303 цена, купить Днепр </a>
+								<p class='old-price'><span>8 600</span></p>
+								<p class='price orange'><span>5 200</span></p>
+							</div>
+						</div>
+						<div class="col">
+							<div class='tovar-item'>
+								<a href='#'>
+									<div>
+										<img src='img/12345.png'/>
+									</div>
+								</a>
+								<a href='#' class="item-title">Газовый клапан 820 NOVA mv. 0.820.303 цена, купить Днепр</a>
+								<p class='old-price'><span>8 600</span></p>
+								<p class='price orange'><span>5 200</span></p>
+							</div>
+						</div>
+						<div class="col">
+							<div class='tovar-item'>
+								<a href='#'>
+									<div>
+										<img src='img/5665220.png'/>
+									</div>
+								</a>
+								<a href='#' class="item-title">Газовый клапан 820 NOVA mv. 0.820.303 цена, купить Днепр</a>
+								<p class='old-price'><span>8 600</span></p>
+								<p class='price orange'><span>5 200</span></p>
+							</div>
+						</div>
+					</div>
+					<div class="clear"></div>
+					<!-- Хиты продаж. Конец -->
+					<div class="content-part">
+						<h3>Добро пожаловать на сайт компании CityGaz Service </h3>
+						<p>CityGaz Service – организация созданная в 2004 году при участии зарубежных производителей газового оборудования и их генеральных дистрибьюторов в России. Компании-производители обеспокоены невысоким ростом темпа продаж своего оборудования на нашем рынке.</p>
+
+						<p>В результате специального изучения ситуации было выяснено, что российский покупатель, не смотря на высокое качество импортной техники, не торопиться отдавать ей предпочтение, беспокоясь о правильной ее установке и грамотном сервисном обслуживании. Люди попросту бояться остаться с техникой один на один, не надеясь на специалистов городских газовых служб и фирменные сервисы, которые грамотно работают только в столице.</p>
+
+						<p>В результате было принято решение на базе одной из крупнейших негосударственных компаний, работающих с газовой техникой создать сервисный центр нового уровня.</p>
+
+						<p>CityGaz Service не является структурным подразделением ни одного из производителей, но имеет всю необходимую техническую базу и мастеров, пошедших обучение за границей. Все ведущие производители выдали новой службе авторизацию на работу с их оборудованием. Свое доверие компании выказали магазины и торговые сети, специализирующиеся на газовом оборудовании. Одним из наших партнеров является компания «ИЗОТ».</p>
+
+						<p>Создание CityGaz Service в Нижнем Новгороде – это первый шаг. Выбор города был осуществлен по двум причинам: во-первых Нижегородский регион один из самых крупных потребителей газа в России; во-вторых, здесь имеется команда специалистов, на базе которой было возможно реализовать проект CGS.</p>
+
+						<p>За длительный период нашей деятельности с 2004 по 2012 год количество сотрудников компании выросло примерно в 2 раза. Специалисты CityGaz Service проходят индивидуальное обучение и ежегодную аттестацию, а руководители ежегодно аттестуются по промышленной безопасности.</p>
+
+						<p>Сегодня компания имеет прямой контакт со всеми ведущими производителями газового оборудования. Практически все известные на рынке марки техники прошли через руки наших специалистов, накопивших уникальный опыт.</p>
+
+						<p>Однако, не менее важным для нас является доверие клиентов. Люди и компании, обратившиеся к нам за услугой проведения пусконаладочных работ либо абонентского обслуживания, вправе рассчитывать на нашу поддержку в приоритетном порядке. Многие наши клиенты возвращаются к нам неоднократно, высоко оценивая надежность компании и квалификацию персонала. Для повышение качества обслуживания на нашем сайте действует сервис обратной связи с руководством компании, где каждый может оставить свой отзыв.</p>
+					</div>
+				</div>
+			</div> 
+			<div class="clear"></div>
 		</div>
-	<ul class="mobile-main-menu">
-		<li><a href="#">О компании</a></li>
-		<li><a href="#">Монтаж</a></li>
-		<li><a href="#">Ремонт и обслуживаение</a></li>
-		<li><a href="#">Как заказать</a></li>
-		<li class="active">
-			<a href="#" class="active-bg">Доставка и оплата</a>
-			<ul class="main-manu2">
-				<li><a href="#">Доставка курьером</a></li>
-				<li><a href="#">Доставка почтой</a></li>
-				<li><a href="#">Самовывоз</a></li>
-			</ul>
-		</li>
-		<li><a href="#">Документы</a></li>
-		<li><a href="#">Контакты</a></li>
-	</ul>
-
-</div>
+		<div class="footer">
+			<?php require_once('includes/footer_top.php'); ?>
+			<?php require_once('includes/footer.php'); ?> 
+		</div>
+	</div>
+</body>
+</html>
 
 <script src="../libs/jquery/dist/jquery.js"></script>
 <script src="../js/popup.js"></script>

a306a700445c2ee65666d3b029bbade4418f8a9e
Kirill Solovyev <Kirill.Solovyev.98@gmail.com>
2017-06-30 20:05:38 +0600

    g

--- includes/header_top.php
+++ includes/header_top.php
@@ -18,8 +18,8 @@
 						ул. Коммунистическая д. 77
 					</div>
 					<div class="contact-phones">
-						диспетчер: <span>(831) <strong>4 - 139 - 169</strong></span><br>
-						тел/факс &nbsp;&nbsp;&nbsp;<span>(831) <strong>202 - 90 - 33</strong></span>
+						диспетчер: <span>(831) 4 - 139 - 169</span><br>
+						тел/факс &nbsp;&nbsp;&nbsp;<span>(831) 202 - 90 - 33</span>
 					</div>
 				</div>
 			</div>

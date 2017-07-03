526ec2fe69f2f50d943f226a4fa9b5aea1bda199
kvsemenoff <kvsemenoff@mail.ru>
2017-07-03 17:59:12 +0400

    d

--- js/fonts.js
+++ js/fonts.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-
+	$("head").append('<link href="https://fonts.googleapis.com/css?family=Roboto:100,300i,400,500,500i,700,700i,900" rel="stylesheet">');
 	$("head").append("<link href='//netdna.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.css' rel='stylesheet'>");
 }); 

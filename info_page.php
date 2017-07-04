fef597544ebac096e579f7bee3a7ec15c19afb84
engine77 <zaur.1kager@gmail.com>
2017-07-04 15:22:03 +0400

    product

--- info_page.php
+++ info_page.php
@@ -276,42 +276,8 @@
 					</div>
 
 					<!--end mobile catalog  -->
-					<div class="clear"></div>
+					<div class="clear"></div>				
 					
-					<!-- buttons on mobile -->
-
-					<div class="catalog__buttons_mobilepage">
-						<div class="sidebar-buttons">
-							<a href="#">Продажа газового оборудования </a>
-							<a href="#compl" data-modal="modal" >Жалобы и предложения клиентов</a>
-						</div>
-
-						<div class="sidebar-ourjob">
-							<div class="sidebar-ourjob__box">
-								<span class="sidebar-ourjob__absolute">1</span>
-								<h4>Полный комплекс <br> работ</h4>
-								<p>Компания CityGaz является членом саморегулируемой организации (СРО), имеет допуск к выполнению полного комплекса работ с газовым оборудованием и газовыми сетями.</p>
-							</div>
-							<div class="sidebar-ourjob__box">
-								<span class="sidebar-ourjob__absolute">2</span>
-								<h4>Работа с любой <br> газовой техникой</h4>
-								<p>CityGaz авторизован ведущими европейскими производителями оборудования и работает практически с любой газовой техникой иностранного и отечественного производства.</p>
-							</div>
-							<div class="sidebar-ourjob__box">
-								<span class="sidebar-ourjob__absolute">3</span>
-								<h4>Наша миссия </h4>
-								<p>Миссия CityGaz service – давать людям спокойствие и уверенность в повседневной жизни. Сотрудники компании в любой момент готовы прийти на помощь и наполнить дом теплом и спокойствием.</p>
-							</div>
-							<div class="sidebar-ourjob__box">
-								<span class="sidebar-ourjob__absolute">4</span>
-								<h4>Широкий выбор газового <br> оборудования</h4>
-								<p>Ресурсы компании не ограничиваются квалифицированным персоналом. CityGaz - это широкий выбор газового оборудования и запчастей, это собственный парк автомобилей, оперативность.</p>
-							</div>
-						</div>
-
-					</div>		
-					
-					<!-- end buttons mobile -->
 
 					<div class="content-part">
 						<h1>Добро пожаловать на сайт компании CityGaz Service </h1>
@@ -346,166 +312,205 @@
 						<h2>В компании поэтапно внедряются ресурсосберегающие и природоохранные технологии.</h2>
 						<p>
 							<img src='img/info-img.jpg' />
-								В результате специального изучения ситуации было выяснено, что российский покупатель, не смотря на высокое качество импортной техники, не торопиться отдавать ей предпочтение, беспокоясь о правильной ее установке и грамотном сервисном обслуживании. Люди попросту бояться остаться с техникой один на один, не надеясь на специалистов городских <a href='#'>после нажатия и при наведении</a> и фирменные сервисы, которые грамотно работают только в столице.
-								<a href='#'>CityGaz Service</a> не является структурным подразделением ни
+							В результате специального изучения ситуации было выяснено, что российский покупатель, не смотря на высокое качество импортной техники, не торопиться отдавать ей предпочтение, беспокоясь о правильной ее установке и грамотном сервисном обслуживании. Люди попросту бояться остаться с техникой один на один, не надеясь на специалистов городских <a href='#'>после нажатия и при наведении</a> и фирменные сервисы, которые грамотно работают только в столице.
+							<a href='#'>CityGaz Service</a> не является структурным подразделением ни
 						</p>
 						<p>Люди попросту бояться остаться с техникой один на один, не надеясь на специалистов городских газовых служб и фирменные сервисы, которые грамотно работают только в столице.<br />
-						<a href='#'>CityGaz Service</a> не является структурным подразделением ни одного из производителей, но имеет всю необходимую техническую базу и мастеров, пошедших обучение за границей. Все ведущие производители выдали новой службе авторизацию на работу с их оборудованием. Свое доверие компании выказали магазины и торговые сети, специализирующиеся на газовом оборудовании. Одним из наших партнеров является компания «ИЗОТ».</p>
-						<div class="clearfix"></div>
+							<a href='#'>CityGaz Service</a> не является структурным подразделением ни одного из производителей, но имеет всю необходимую техническую базу и мастеров, пошедших обучение за границей. Все ведущие производители выдали новой службе авторизацию на работу с их оборудованием. Свое доверие компании выказали магазины и торговые сети, специализирующиеся на газовом оборудовании. Одним из наших партнеров является компания «ИЗОТ».</p>
+							<div class="clearfix"></div>
 
-						<h3>Таблица комплектующих изделий  </h3>
-						<table class="info_page_table">
-							<thead>
-								<tr>
-									<th>Описание</th>
-									<th>Цена</th>
-									<th>Скидка</th>
-								</tr>
-							</thead>
-							<tbody>
-								<tr>
-									<td>Дизельный двигатель</td>
-									<td>15700.-</td>
-									<td>30%</td>
-								</tr>
-								<tr>
-									<td>Бензиновый двигатель, непосредственный впрыск</td>
-									<td>20800 .-</td>
-									<td>45%</td>
-								</tr>
-								<tr>
-									<td>Бензиновые двигатели, прямой впрыск</td>
-									<td>15700.-</td>
-									<td>15%</td>
-								</tr>
-							</tbody>
-						</table>
+							<h3>Таблица комплектующих изделий  </h3>
+							<table class="info_page_table">
+								<thead>
+									<tr>
+										<th>Описание</th>
+										<th>Цена</th>
+										<th>Скидка</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td>Дизельный двигатель</td>
+										<td>15700.-</td>
+										<td>30%</td>
+									</tr>
+									<tr>
+										<td>Бензиновый двигатель, непосредственный впрыск</td>
+										<td>20800 .-</td>
+										<td>45%</td>
+									</tr>
+									<tr>
+										<td>Бензиновые двигатели, прямой впрыск</td>
+										<td>15700.-</td>
+										<td>15%</td>
+									</tr>
+								</tbody>
+							</table>
 
-						<h2>Технические документы</h2>
-						<ul class="tech-doc">
-							<li><span>Технические паспорта для чайников</span>
-								<ul class="tech-doc__inside">
-								<li> <span>Тех. Паспорта для котлов фирмы Liberty</span>
-										<ul class="tech-doc__inside-links">
-											<li><a href="#">Паспорт для котла Baxi модель 45235345</a></li>
-											<li><a href="#">Паспорт для котла Baxi модель 87854658</a></li>
-										</ul>
-									</li>
-									<li> <span>Тех. Паспорта для котлов фирмы Bax</span>
-										<ul class="tech-doc__inside-links">
-											<li><a href="#">Паспорт для котла Baxi модель 45235345</a></li>
-											<li><a href="#">Паспорт для котла Baxi модель 87854658</a></li>
-										</ul>
-									</li>
-								</ul>
-							</li>	
-							<li><span>Технические паспорта для бойлеров</span>
-								<ul class="tech-doc__inside">
-								<li> <span>Тех. Паспорта для котлов фирмы Liberty</span>
-										<ul class="tech-doc__inside-links">
-											<li><a href="#">Паспорт для котла Baxi модель 45235345</a></li>
-											<li><a href="#">Паспорт для котла Baxi модель 87854658</a></li>
-										</ul>
-									</li>
-									<li> <span>Тех. Паспорта для котлов фирмы Bax</span>
-										<ul class="tech-doc__inside-links">
-											<li><a href="#">Паспорт для котла Baxi модель 45235345</a></li>
-											<li><a href="#">Паспорт для котла Baxi модель 87854658</a></li>
-										</ul>
-									</li>
-								</ul>
-							</li>	
-							<li><span>Технические паспорта для котлов</span>
-								<ul class="tech-doc__inside">
-								<li> <span>Тех. Паспорта для котлов фирмы Liberty</span>
-										<ul class="tech-doc__inside-links">
-											<li><a href="#">Паспорт для котла Baxi модель 45235345</a></li>
-											<li><a href="#">Паспорт для котла Baxi модель 87854658</a></li>
-										</ul>
-									</li>
-									<li> <span>Тех. Паспорта для котлов фирмы Bax</span>
-										<ul class="tech-doc__inside-links">
-											<li><a href="#">Паспорт для котла Baxi модель 45235345</a></li>
-											<li><a href="#">Паспорт для котла Baxi модель 87854658</a></li>
-										</ul>
-									</li>
-								</ul>
-							</li>	
-							<li> <span>Технические паспорта для бойлеров</span>
-								<ul class="tech-doc__inside">
-								<li> <span>Тех. Паспорта для котлов фирмы Liberty</span>
-										<ul class="tech-doc__inside-links">
-											<li><a href="#">Паспорт для котла Baxi модель 45235345</a></li>
-											<li><a href="#">Паспорт для котла Baxi модель 87854658</a></li>
-										</ul>
-									</li>
-									<li> <span>Тех. Паспорта для котлов фирмы Bax</span>
-										<ul class="tech-doc__inside-links">
-											<li><a href="#">Паспорт для котла Baxi модель 45235345</a></li>
-											<li><a href="#">Паспорт для котла Baxi модель 87854658</a></li>
-										</ul>
-									</li>
-								</ul>
-							</li>
-							<li> <span>Технические паспорта для бойлеров</span>
-								<ul class="tech-doc__inside">
-								<li> <span>Тех. Паспорта для котлов фирмы Liberty</span>
-										<ul class="tech-doc__inside-links">
-											<li><a href="#">Паспорт для котла Baxi модель 45235345</a></li>
-											<li><a href="#">Паспорт для котла Baxi модель 87854658</a></li>
-										</ul>
-									</li>
-									<li> <span>Тех. Паспорта для котлов фирмы Bax</span>
-										<ul class="tech-doc__inside-links">
-											<li><a href="#">Паспорт для котла Baxi модель 45235345</a></li>
-											<li><a href="#">Паспорт для котла Baxi модель 87854658</a></li>
-										</ul>
-									</li>
-								</ul>
-							</li>		
-							<li> <span>Технические паспорта для чайников</span>
-								<ul class="tech-doc__inside">
-								<li> <span>Тех. Паспорта для котлов фирмы Liberty</span>
-										<ul class="tech-doc__inside-links">
-											<li><a href="#">Паспорт для котла Baxi модель 45235345</a></li>
-											<li><a href="#">Паспорт для котла Baxi модель 87854658</a></li>
-										</ul>
-									</li>
-									<li> <span>Тех. Паспорта для котлов фирмы Bax</span>
-										<ul class="tech-doc__inside-links">
-											<li><a href="#">Паспорт для котла Baxi модель 45235345</a></li>
-											<li><a href="#">Паспорт для котла Baxi модель 87854658</a></li>
-										</ul>
-									</li>
-								</ul>
-							</li>
-							<li> <span>Технические паспорта для бойлеров</span>
-								<ul class="tech-doc__inside">
-								<li> <span>Тех. Паспорта для котлов фирмы Liberty</span>
-										<ul class="tech-doc__inside-links">
-											<li><a href="#">Паспорт для котла Baxi модель 45235345</a></li>
-											<li><a href="#">Паспорт для котла Baxi модель 87854658</a></li>
-										</ul>
-									</li>
-									<li> <span>Тех. Паспорта для котлов фирмы Bax</span>
-										<ul class="tech-doc__inside-links">
-											<li><a href="#">Паспорт для котла Baxi модель 45235345</a></li>
-											<li><a href="#">Паспорт для котла Baxi модель 87854658</a></li>
-										</ul>
-									</li>
-								</ul>
-							</li>
-						</ul>
+							<h2>Технические документы</h2>
+							<ul class="tech-doc">
+								<li><span>Технические паспорта для чайников</span>
+									<ul class="tech-doc__inside">
+										<li> <span>Тех. Паспорта для котлов фирмы Liberty</span>
+											<ul class="tech-doc__inside-links">
+												<li><a href="#">Паспорт для котла Baxi модель 45235345</a></li>
+												<li><a href="#">Паспорт для котла Baxi модель 87854658</a></li>
+											</ul>
+										</li>
+										<li> <span>Тех. Паспорта для котлов фирмы Bax</span>
+											<ul class="tech-doc__inside-links">
+												<li><a href="#">Паспорт для котла Baxi модель 45235345</a></li>
+												<li><a href="#">Паспорт для котла Baxi модель 87854658</a></li>
+											</ul>
+										</li>
+									</ul>
+								</li>	
+								<li><span>Технические паспорта для бойлеров</span>
+									<ul class="tech-doc__inside">
+										<li> <span>Тех. Паспорта для котлов фирмы Liberty</span>
+											<ul class="tech-doc__inside-links">
+												<li><a href="#">Паспорт для котла Baxi модель 45235345</a></li>
+												<li><a href="#">Паспорт для котла Baxi модель 87854658</a></li>
+											</ul>
+										</li>
+										<li> <span>Тех. Паспорта для котлов фирмы Bax</span>
+											<ul class="tech-doc__inside-links">
+												<li><a href="#">Паспорт для котла Baxi модель 45235345</a></li>
+												<li><a href="#">Паспорт для котла Baxi модель 87854658</a></li>
+											</ul>
+										</li>
+									</ul>
+								</li>	
+								<li><span>Технические паспорта для котлов</span>
+									<ul class="tech-doc__inside">
+										<li> <span>Тех. Паспорта для котлов фирмы Liberty</span>
+											<ul class="tech-doc__inside-links">
+												<li><a href="#">Паспорт для котла Baxi модель 45235345</a></li>
+												<li><a href="#">Паспорт для котла Baxi модель 87854658</a></li>
+											</ul>
+										</li>
+										<li> <span>Тех. Паспорта для котлов фирмы Bax</span>
+											<ul class="tech-doc__inside-links">
+												<li><a href="#">Паспорт для котла Baxi модель 45235345</a></li>
+												<li><a href="#">Паспорт для котла Baxi модель 87854658</a></li>
+											</ul>
+										</li>
+									</ul>
+								</li>	
+								<li> <span>Технические паспорта для бойлеров</span>
+									<ul class="tech-doc__inside">
+										<li> <span>Тех. Паспорта для котлов фирмы Liberty</span>
+											<ul class="tech-doc__inside-links">
+												<li><a href="#">Паспорт для котла Baxi модель 45235345</a></li>
+												<li><a href="#">Паспорт для котла Baxi модель 87854658</a></li>
+											</ul>
+										</li>
+										<li> <span>Тех. Паспорта для котлов фирмы Bax</span>
+											<ul class="tech-doc__inside-links">
+												<li><a href="#">Паспорт для котла Baxi модель 45235345</a></li>
+												<li><a href="#">Паспорт для котла Baxi модель 87854658</a></li>
+											</ul>
+										</li>
+									</ul>
+								</li>
+								<li> <span>Технические паспорта для бойлеров</span>
+									<ul class="tech-doc__inside">
+										<li> <span>Тех. Паспорта для котлов фирмы Liberty</span>
+											<ul class="tech-doc__inside-links">
+												<li><a href="#">Паспорт для котла Baxi модель 45235345</a></li>
+												<li><a href="#">Паспорт для котла Baxi модель 87854658</a></li>
+											</ul>
+										</li>
+										<li> <span>Тех. Паспорта для котлов фирмы Bax</span>
+											<ul class="tech-doc__inside-links">
+												<li><a href="#">Паспорт для котла Baxi модель 45235345</a></li>
+												<li><a href="#">Паспорт для котла Baxi модель 87854658</a></li>
+											</ul>
+										</li>
+									</ul>
+								</li>		
+								<li> <span>Технические паспорта для чайников</span>
+									<ul class="tech-doc__inside">
+										<li> <span>Тех. Паспорта для котлов фирмы Liberty</span>
+											<ul class="tech-doc__inside-links">
+												<li><a href="#">Паспорт для котла Baxi модель 45235345</a></li>
+												<li><a href="#">Паспорт для котла Baxi модель 87854658</a></li>
+											</ul>
+										</li>
+										<li> <span>Тех. Паспорта для котлов фирмы Bax</span>
+											<ul class="tech-doc__inside-links">
+												<li><a href="#">Паспорт для котла Baxi модель 45235345</a></li>
+												<li><a href="#">Паспорт для котла Baxi модель 87854658</a></li>
+											</ul>
+										</li>
+									</ul>
+								</li>
+								<li> <span>Технические паспорта для бойлеров</span>
+									<ul class="tech-doc__inside">
+										<li> <span>Тех. Паспорта для котлов фирмы Liberty</span>
+											<ul class="tech-doc__inside-links">
+												<li><a href="#">Паспорт для котла Baxi модель 45235345</a></li>
+												<li><a href="#">Паспорт для котла Baxi модель 87854658</a></li>
+											</ul>
+										</li>
+										<li> <span>Тех. Паспорта для котлов фирмы Bax</span>
+											<ul class="tech-doc__inside-links">
+												<li><a href="#">Паспорт для котла Baxi модель 45235345</a></li>
+												<li><a href="#">Паспорт для котла Baxi модель 87854658</a></li>
+											</ul>
+										</li>
+									</ul>
+								</li>
+							</ul>
+
+
+
+
+						</div>
+
+						<!-- buttons on mobile -->
+
+						<div class="catalog__buttons_mobilepage">
+							<div class="sidebar-buttons">
+								<a href="#">Продажа газового оборудования </a>
+								<a href="#compl" data-modal="modal" >Жалобы и предложения клиентов</a>
+							</div>
+
+							<div class="sidebar-ourjob">
+								<div class="sidebar-ourjob__box">
+									<span class="sidebar-ourjob__absolute">1</span>
+									<h4>Полный комплекс <br> работ</h4>
+									<p>Компания CityGaz является членом саморегулируемой организации (СРО), имеет допуск к выполнению полного комплекса работ с газовым оборудованием и газовыми сетями.</p>
+								</div>
+								<div class="sidebar-ourjob__box">
+									<span class="sidebar-ourjob__absolute">2</span>
+									<h4>Работа с любой <br> газовой техникой</h4>
+									<p>CityGaz авторизован ведущими европейскими производителями оборудования и работает практически с любой газовой техникой иностранного и отечественного производства.</p>
+								</div>
+								<div class="sidebar-ourjob__box">
+									<span class="sidebar-ourjob__absolute">3</span>
+									<h4>Наша миссия </h4>
+									<p>Миссия CityGaz service – давать людям спокойствие и уверенность в повседневной жизни. Сотрудники компании в любой момент готовы прийти на помощь и наполнить дом теплом и спокойствием.</p>
+								</div>
+								<div class="sidebar-ourjob__box">
+									<span class="sidebar-ourjob__absolute">4</span>
+									<h4>Широкий выбор газового <br> оборудования</h4>
+									<p>Ресурсы компании не ограничиваются квалифицированным персоналом. CityGaz - это широкий выбор газового оборудования и запчастей, это собственный парк автомобилей, оперативность.</p>
+								</div>
+							</div>
+
+						</div>		
+						
+						<!-- end buttons mobile -->
 					</div>
-				</div>
-			</div> 
-			<div class="clear"></div>
+				</div> 
+				<div class="clear"></div>
+			</div>
+			<div class="footer">
+				<?php require_once('includes/footer_top.php'); ?>
+				<?php require_once('includes/footer.php'); ?> 
+			</div>
 		</div>
-		<div class="footer">
-			<?php require_once('includes/footer_top.php'); ?>
-			<?php require_once('includes/footer.php'); ?> 
-		</div>
-	</div>
-</body>
-</html>
+	</body>
+	</html>
 
